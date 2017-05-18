8ffaea9c9528ee189ec86d11411bfa23e6cf53b7
bloodaxed <bloodaxed@gmail.com>
2017-05-18 12:35:41 -0500

    initial

---
+++ members.go
@@ -0,0 +1,63 @@
+package main
+
+import "net/http"
+import "fmt"
+import "io/ioutil"
+import "encoding/json"
+//import "os"
+import str "strings"
+import "time"
+import "flag"
+
+func main(){
+	type Member struct {
+		Name string
+		Rank string
+		Joined string
+	}
+
+	
+	var when string
+	whenParsed := time.Now()
+
+	//get the rank argument
+	flag.StringVar(&when, "date", "default", "a date in YYYY-MM-DD format")
+	
+	flag.Parse()
+	rank := string(flag.Arg(0))
+
+	fmt.Println(when)
+	fmt.Println(rank)
+
+	if when == "default"{
+		whenParsed = time.Now()
+	} else{	
+		whenParsed, _ = time.Parse("2006-01-02", when)
+	}
+	fmt.Println(whenParsed)
+
+	var m []Member
+
+	resp, err := http.Get("https://api.guildwars2.com/v2/guild/6E313C3A-02E3-4170-88D4-F1709BAE0F1A/members?access_token=A08665C2-B7AF-924D-ADD5-E3AB30024025E9D8C4B3-0097-46EC-9377-A19BBCA5BDC6")
+	if err != nil {
+		
+	}
+
+	defer resp.Body.Close()	
+	if resp.StatusCode == 200{
+		body, _ := ioutil.ReadAll(resp.Body)
+		//bodyString := string(body)
+		err := json.Unmarshal(body, &m)
+		if err != nil{
+			fmt.Println("error", err)
+		}
+		for _, x := range m{
+			tmp, _ := time.Parse(time.RFC3339, x.Joined)
+			if str.ToLower(x.Rank) == str.ToLower(rank){
+				if tmp.Before(whenParsed){
+					fmt.Printf("%+v\n", x)
+				}
+			}
+		}
+	}
+}
